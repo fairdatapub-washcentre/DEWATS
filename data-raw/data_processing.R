b4c2508d1b25d1ec067516b81be455cd623e4ed1
XabelaS <sinethembaxabela@gmail.com>
2024-11-28 14:26:10 +0000

    Imported data

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -12,6 +12,9 @@ library(openxlsx)
 
 # Read data --------------------------------------------------------------------
 # data_in <- readr::read_csv("data-raw/dataset.csv")
+data_in <-    read_excel("data-raw/Field Data Collection for Wastewater Characterization.xlsx")
+
+
 # codebook <- readxl::read_excel("data-raw/codebook.xlsx") |>
 #  clean_names()
 
